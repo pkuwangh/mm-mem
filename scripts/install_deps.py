af0dcba6bd579d8636791f4f4548517918894d72
Hao Wang <pkuwangh@gmail.com>
2022-09-20 02:30:04 -0700

    add support on debian/ubuntu

--- scripts/install_deps.py
+++ scripts/install_deps.py
@@ -7,14 +7,19 @@ import shutil
 cmd = []
 
 if platform.system() == "Linux":
-    pkg_list = ["gcc", "gcc-c++", "cmake3", "numactl-devel", "boost-devel"]
     if shutil.which("dnf"):
         pkg_manager = "dnf"
     elif shutil.which("yum"):
         pkg_manager = "yum"
+    elif shutil.which("apt"):
+        pkg_manager = "apt"
     else:
-        print("dnf | yum not available?!")
+        print("dnf | yum | apt not available?!")
         exit(1)
+    if pkg_manager == "dnf" or pkg_manager == "yum":
+        pkg_list = ["gcc", "gcc-c++", "cmake3", "numactl-devel", "boost-devel"]
+    elif pkg_manager == "apt":
+        pkg_list = ["gcc-10", "g++-10", "cmake", "libnuma-dev", "libboost-program-options-dev"]
     cmd = [pkg_manager, "install", "-y"] + pkg_list
 
 elif platform.system() == "Darwin":
@@ -26,4 +31,3 @@ else:
     exit(1)
 
 os.system(" ".join(cmd))
-
