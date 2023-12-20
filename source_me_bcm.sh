4f444f8cb9d2713b9426c186593a9017d1ddf1ee
Hao Wang <pkuwangh@gmail.com>
2023-12-19 16:12:46 -0800

    [build] improve env setting for bcm

--- source_me_bcm.sh
+++ source_me_bcm.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# alias
+alias lt='ls -lhrt'
+
+# utils
 infoMsg() {
     echo -e "\033[32;1m${1}\033[0m"
 }
@@ -12,30 +16,36 @@ checkMsg() {
     echo -e "\033[33;4m${1}\033[0m"
 }
 
-alias lt='ls -lhrt'
+checkMsg "Before ..."
+echo $CPATH
+echo $LIBRARY_PATH
+echo $LD_LIBRARY_PATH
 
-CM_SHARED_BOOST_PATH="/cm/shared/apps/boost/current"
-CM_LOCAL_BOOST_PATH="/cm/local/apps/boost/current"
-
-if [ -d $CM_SHARED_BOOST_PATH ]; then
-    BOOST_PATH=$CM_SHARED_BOOST_PATH
-elif [ -d $CM_LOCAL_BOOST_PATH ]; then
-    BOOST_PATH=$CM_LOCAL_BOOST_PATH
-else
-    warnMsg "boost not found in shared place; try `module load boost`?"
-    return
+# boost
+if ! [[ "${LD_LIBRARY_PATH}" =~ "boost" ]]; then
+    CM_SHARED_BOOST_PATH="/cm/shared/apps/boost/current"
+    CM_LOCAL_BOOST_PATH="/cm/local/apps/boost/current"
+    if [ -d $CM_SHARED_BOOST_PATH ]; then
+        CM_BOOST_PATH=$CM_SHARED_BOOST_PATH
+    elif [ -d $CM_LOCAL_BOOST_PATH ]; then
+        CM_BOOST_PATH=$CM_LOCAL_BOOST_PATH
+    else
+        warnMsg "boost not found in shared place; try 'module load boost'?"
+    fi
+    if [ -n "${CM_BOOST_PATH}" ]; then
+        infoMsg "Found boost at ${CM_BOOST_PATH}/"
+        # for cmake to use boost
+        export BOOST_ROOT="${CM_BOOST_PATH}"
+        export BOOST_INCLUDEDIR="${CM_BOOST_PATH}/include"
+        export BOOST_LIBRARYDIR="${CM_BOOST_PATH}/lib64"
+        # system paths
+        export CPATH="${CM_BOOST_PATH}/include:${CPATH}"
+        export LIBRARY_PATH="${CM_BOOST_PATH}/lib64:${LIBRARY_PATH}"
+        export LD_LIBRARY_PATH="${CM_BOOST_PATH}/lib64:${LD_LIBRARY_PATH}"
+    fi
 fi
-infoMsg "Found boost at ${BOOST_PATH}/"
 
-CUDA_PATH="/cm/shared/apps/cuda-latest/toolkit/current"
-infoMsg "Found cuda at ${CUDA_PATH}/"
-
-export BOOST_ROOT="${BOOST_PATH}"
-export BOOST_INCLUDEDIR="${BOOST_PATH}/include"
-export BOOST_LIBRARYDIR="${BOOST_PATH}/lib64"
-export CUDACXX="${CUDA_PATH}/bin/nvcc"
-
-export CPATH="${BOOST_PATH}/include:${CUDA_PATH}/include"
-export LIBRARY_PATH="${BOOST_PATH}/lib64:${CUDA_PATH}/lib64"
-export LD_LIBRARY_PATH="${BOOST_PATH}/lib64:${CUDA_PATH}/lib64"
-
+checkMsg "After ..."
+echo $CPATH
+echo $LIBRARY_PATH
+echo $LD_LIBRARY_PATH
